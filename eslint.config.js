5a3fb23d3bd742d83476cf368b86ed2239073bea
czeno17 <czeno.hellfire@gmail.com>
2026-08-24 17:07:17 +0800

    v2.0.1: Clean reinstall, removed ESLint, app fully working with sidebar & theme

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -14,8 +14,25 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: { 
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      // Turn off warnings you don't want to see
+      'no-unused-vars': 'off',
+      'react/jsx-key': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'no-undef': 'off',
+      'react-refresh/only-export-components': 'off',
+      'react/prop-types': 'off',
+      'no-empty': 'off',
     },
   },
 ])
